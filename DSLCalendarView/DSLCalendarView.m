07fc23fa82860932750784c8c536711c109dade8
Daniel Sykes-Turner <dst@localz.co>
2014-03-02 20:00:00 +1100

    Initial load resizing bug fixed

--- DSLCalendarView/DSLCalendarView.m
+++ DSLCalendarView/DSLCalendarView.m
@@ -117,6 +117,13 @@
 
     [self updateMonthLabelMonth:_visibleMonth];
     [self positionViewsForMonth:_visibleMonth fromMonth:_visibleMonth animated:NO];
+    
+    //wait for everything else to load, then refresh the view size
+    double delayInSeconds = 0.01;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self setVisibleMonth:self.visibleMonth animated:YES];
+    });
 }
 
 
